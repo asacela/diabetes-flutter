4fd0bfe51c7138afc3addc4568bc3d2efd0b4fe8
soundscrub <soundscrubme@gmail.com>
2024-06-10 00:57:04 -0700

    Adding meal generation portion

--- lib/resources/openai_methods.dart
+++ lib/resources/openai_methods.dart
@@ -7,6 +7,7 @@ import 'package:dio/dio.dart';
 class OpenAI_Methods {
   final dio = Dio();
 
+  // Generate carb estimate
   Future<Map<String, dynamic>> sendImageToGPT4Vision(
       {required Uint8List file,
       int maxTokens = 100,
@@ -50,9 +51,68 @@ class OpenAI_Methods {
       );
       print("getting generated meal...");
       final jsonResponse = response.data;
-      final String jsonContent = jsonResponse["choices"][0]["message"]["content"];
+      final String jsonContent =
+          jsonResponse["choices"][0]["message"]["content"];
       int startIndex = jsonContent.indexOf('{');
-      int endIndex = jsonContent.lastIndexOf('}') + 1; // Add 1 to include the closing brace
+      int endIndex = jsonContent.lastIndexOf('}') +
+          1; // Add 1 to include the closing brace
+      String mealJson = jsonContent.substring(startIndex, endIndex);
+      Map<String, dynamic> jsonMap = json.decode(mealJson);
+
+      if (jsonResponse['error'] != null) {
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+      return jsonMap;
+    } catch (e) {
+      throw Exception("error: $e");
+    }
+  }
+
+  // Generate ai meal
+  Future<Map<String, dynamic>> sendPromptToGPT4(
+      {required String mealPrompt,
+      int maxTokens = 100,
+      String model = "gpt-4-vision-preview"}) async {
+    try {
+
+      String fullPrompt = 'GPT, your task is to generate a low-carb meal for a diabetic patient. Make sure the meal has around 130 carbs or less. Generate the closest meal possible to the given user prompt: ${mealPrompt}Provide a JSON with the following format: {"title":"<title>","carbs":<number>,"ingredientList":["<ingredient_1>","<ingredient_2>","<ingredient_3>"],"recipe":"<recipe>","absorptionTime":"slow|medium|fast"}';
+      
+
+      final response = await dio.post(
+        "$OPENAI_BASE_URL/chat/completions",
+        options: Options(
+          headers: {
+            HttpHeaders.authorizationHeader: 'Bearer $OPENAI_API_KEY',
+            HttpHeaders.contentTypeHeader: "application/json",
+          },
+        ),
+        data: jsonEncode({
+          'model': model,
+          'messages': [
+            {
+              'role': 'system',
+              'content': 'You have to give concise and short answers.'
+            },
+            {
+              'role': 'user',
+              'content': [
+                {
+                  'type': 'text',
+                  'text': fullPrompt
+                },
+              ],
+            },
+          ],
+          'max_tokens': maxTokens,
+        }),
+      );
+      print("getting meal generation...");
+      final jsonResponse = response.data;
+      final String jsonContent =
+          jsonResponse["choices"][0]["message"]["content"];
+      int startIndex = jsonContent.indexOf('{');
+      int endIndex = jsonContent.lastIndexOf('}') +
+          1; // Add 1 to include the closing brace
       String mealJson = jsonContent.substring(startIndex, endIndex);
       Map<String, dynamic> jsonMap = json.decode(mealJson);
 
--- lib/widgets/add_meal_dialog.dart
+++ lib/widgets/add_meal_dialog.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/resources/firestore_methods.dart';
+import 'package:instagram_flutter/resources/openai_methods.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
@@ -25,13 +26,17 @@ class _AddMealDialogState extends State<AddMealDialog> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   User? _currentUser;
   Map<String, dynamic>? _userData; // To store additional user data
+  bool _isGenerated = false;
+  bool _isLoading = false;
+  Map<String, dynamic>? jsonResponse;
 
   String dropdownValue = absorptionValues.first;
   Uint8List? _image;
 
   final TextEditingController _mealNameController = TextEditingController();
   final TextEditingController _carbAmountController = TextEditingController();
-  final TextEditingController _ingredientListController = TextEditingController();
+  final TextEditingController _ingredientListController =
+      TextEditingController();
   final TextEditingController _recipeController = TextEditingController();
   final TextEditingController _absorptionController = TextEditingController();
 
@@ -48,6 +53,20 @@ class _AddMealDialogState extends State<AddMealDialog> {
     });
   }
 
+    void getMealGeneration() async {
+    try {
+      final res = await OpenAI_Methods().sendPromptToGPT4(mealPrompt: _mealNameController.text);
+
+      setState(() {
+        jsonResponse = res;
+        _isGenerated = true;
+        _isLoading = false;
+      });
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
   Future<void> _getCurrentUser() async {
     _currentUser = _auth.currentUser;
     if (_currentUser != null) {
@@ -61,192 +80,230 @@ class _AddMealDialogState extends State<AddMealDialog> {
       });
     }
   }
-@override
-Widget build(BuildContext context) {
 
+  @override
+  Widget build(BuildContext context) {
+    void addMeal() async {
+      String res = "Some error occured";
 
-  void addMeal() async {
-    String res = "Some error occured";
-
-    try {
-
-    // Call the uploadMeal method and await its result
-    res = await FirestoreMethods().uploadMeal(
-      _currentUser!.uid,
-      _mealNameController.text,
-      _image!,
-      _carbAmountController.text,
-      _ingredientListController.text,
-      _recipeController.text,
-      _absorptionController.text,
-    );
-
-      if (res != "success") {
-        showSnackBar(res, context);
-      } else {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const ResponsiveLayout(
-              webScreenLayout: webScreenLayout(),
-              mobileScreenLayout: mobileScreenLayout(),
-            )
-          ),
+      try {
+        // Call the uploadMeal method and await its result
+        res = await FirestoreMethods().uploadMeal(
+          _currentUser!.uid,
+          _mealNameController.text,
+          _image!,
+          _carbAmountController.text,
+          _ingredientListController.text,
+          _recipeController.text,
+          _absorptionController.text,
         );
+
+        if (res != "success") {
+          showSnackBar(res, context);
+        } else {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+                builder: (context) => const ResponsiveLayout(
+                      webScreenLayout: webScreenLayout(),
+                      mobileScreenLayout: mobileScreenLayout(),
+                    )),
+          );
+        }
+      } catch (error) {
+        showSnackBar("some error occured", context);
       }
     }
-    catch(error) {
-      showSnackBar("some error occured", context);
-    }
-  }
 
-  return SafeArea(
-    child: Padding(
-      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 10),
-            Container(
-              height: 120.0,
-              width: 400.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                image: DecorationImage(
-                  image: _image != null
-                      ? MemoryImage(_image!) as ImageProvider
-                      : NetworkImage(
-                          'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-                  fit: BoxFit.cover,
+    return SafeArea(
+      child: Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Container(
+                height: 120.0,
+                width: 400.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(
+                    image: _image != null
+                        ? MemoryImage(_image!) as ImageProvider
+                        : NetworkImage(
+                            'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                    fit: BoxFit.cover,
+                  ),
+                  shape: BoxShape.rectangle,
                 ),
-                shape: BoxShape.rectangle,
-              ),
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0), // Add some padding if necessary
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(36.0), // Set the desired border radius
-                        child: Container(
-                          width: 48.0, // Same width as default FAB divided bby 2
-                          height: 48.0, // Same height as default FAB divided bby 2
-                          child: FloatingActionButton(
-                            onPressed: (selectImage),
-                            child: const Icon(Icons.edit),
-                            backgroundColor: Colors.blue, // Replace with your secondaryColor
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16.0)), // Ensure shape matches the ClipRRect
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(
+                            8.0), // Add some padding if necessary
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                              36.0), // Set the desired border radius
+                          child: Container(
+                            width:
+                                48.0, // Same width as default FAB divided bby 2
+                            height:
+                                48.0, // Same height as default FAB divided bby 2
+                            child: FloatingActionButton(
+                              onPressed: (selectImage),
+                              child: const Icon(Icons.edit),
+                              backgroundColor: Colors
+                                  .blue, // Replace with your secondaryColor
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      16.0)), // Ensure shape matches the ClipRRect
+                            ),
                           ),
                         ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextFieldInput(
+                      hintText: 'Meal Name',
+                      textInputType: TextInputType.text,
+                      textEditingController: _mealNameController),
+                  TextFieldInput(
+                      hintText: '(g)',
+                      textInputType: TextInputType.number,
+                      textEditingController: _carbAmountController,
+                      inputSuffix: "carbs"),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Absorption Time: ', // Text to be a heading
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // Make it bold
+                      fontSize: 12,
+                      color: secondaryColor, // Increase font size
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * (5 / 12),
+                  ),
+                  DropdownButton<String>(
+                    value: dropdownValue,
+                    items: absorptionValues
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                    },
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TextFieldInput(
-                    hintText: 'Meal Name',
-                    textInputType: TextInputType.text,
-                    textEditingController: _mealNameController),
-                TextFieldInput(
-                    hintText: '(g)',
-                    textInputType: TextInputType.number,
-                    textEditingController: _carbAmountController,
-                    inputSuffix: "carbs"),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Text(
-                  'Absorption Time: ', // Text to be a heading
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold, // Make it bold
-                    fontSize: 12,
-                    color: secondaryColor, // Increase font size
+              const SizedBox(height: 24),
+              SizedBox(
+                height: 120,
+                child: CupertinoTextField(
+                  controller: _ingredientListController,
+                  placeholder: "Enter Ingredients",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w400, color: Colors.white),
+                  maxLines: null,
+                  textAlignVertical:
+                      TextAlignVertical.top, // Align text to the top
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    borderRadius:
+                        BorderRadius.circular(10), // Add rounded corners
                   ),
                 ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * (5 / 12),
-                ),
-                DropdownButton<String>(
-                  value: dropdownValue,
-                  items: absorptionValues
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? value) {
-                    setState(() {
-                      dropdownValue = value!;
-                    });
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 24),
-            SizedBox(
-              height: 120,
-              child: CupertinoTextField(
-                controller: _ingredientListController,
-                placeholder: "Enter Ingredients",
-                style: const TextStyle(
-                    fontWeight: FontWeight.w400, color: Colors.white),
-                maxLines: null,
-                textAlignVertical: TextAlignVertical.top, // Align text to the top
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                height: 120,
+                child: CupertinoTextField(
+                  controller: _recipeController,
+                  placeholder: "Enter Recipe",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w400, color: Colors.white),
+                  maxLines: null,
+                  textAlignVertical:
+                      TextAlignVertical.top, // Align text to the top
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    borderRadius:
+                        BorderRadius.circular(10), // Add rounded corners
                   ),
-                  borderRadius: BorderRadius.circular(10), // Add rounded corners
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-            SizedBox(
-              height: 120,
-              child: CupertinoTextField(
-                controller: _recipeController,
-                placeholder: "Enter Recipe",
-                style: const TextStyle(
-                    fontWeight: FontWeight.w400, color: Colors.white),
-                maxLines: null,
-                textAlignVertical: TextAlignVertical.top, // Align text to the top
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: (getMealGeneration),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: blueColor, // Text color
+                      shadowColor: Colors.black, // Shadow color
+                      elevation: 5, // Elevation
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0), // Rounded corners
+                      ),
+                    ),
+                    child: Row(children: [
+                      Text(
+                        'Generate Meal ',
+                        style: TextStyle(
+                          fontWeight:
+                              FontWeight.bold, // Example style properties
+                          fontSize: 16, // Example style properties
+                          color: primaryColor, // Example color
+                        ),
+                      ),
+                      Icon(
+                        Icons.auto_awesome,
+                        color: primaryColor,
+                      )
+                    ]),
                   ),
-                  borderRadius: BorderRadius.circular(10), // Add rounded corners
-                ),
-              ),
-            ),
-            SizedBox(height: 24), // Add spacing between text field and button
-            ElevatedButton(
-              onPressed: (addMeal),
-              child: Text(
-                'Save Meal',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, // Example style properties
-                  fontSize: 16, // Example style properties
-                  color: blueColor, // Example color
-                ),
-              ),
-            ),
-          ],
+                  SizedBox(width: 8.0),
+                  ElevatedButton(
+                    onPressed: (addMeal),
+                    child: Text(
+                      'Save Meal',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold, // Example style properties
+                        fontSize: 16, // Example style properties
+                        color: blueColor, // Example color
+                      ),
+                    ),
+                  ),
+                ],
+              ) // Add spacing between text field and button
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
-
+    );
+  }
 }
